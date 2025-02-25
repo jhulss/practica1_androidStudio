eed083eae93ecaf5faaae5cad50c93ef1bdb3bf9
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2025-02-24 20:22:45 -0400

    primer commit

---
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.prueba
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.prueba.ui.theme.PruebaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PruebaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PruebaTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun SigninPage() {
+
+    var userSignIn by remember { mutableStateOf("") }
+    var passwordSignIn by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Text(text = "Sign In")
+        TextField(value = userSignIn, onValueChange = { userSignIn = it} )
+        TextField(value = passwordSignIn, onValueChange = { passwordSignIn = it})
+        Button(modifier = Modifier.fillMaxWidth(), onClick = { /*TODO*/ }) {
+            Text(text = "Sign In" )
+        }
+    }
+}
